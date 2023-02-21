31aa3f4a35c8155e256791f9e7c2877e3daba0fe
Dinesh-Patnaik-au28 <94793627+Dinesh-Patnaik-au28@users.noreply.github.com>
2023-02-21 23:30:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import About from './component/About';
+import Home from './component/Home';
+import Contact from './component/Contact';
+
+function App() {
+  return (
+    <div>
+      <h2 className="text-3xl font-bold underline">Welcome to React</h2>
+      {/* <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes> */}
+    </div>
+  )
+}
+
+export default App
